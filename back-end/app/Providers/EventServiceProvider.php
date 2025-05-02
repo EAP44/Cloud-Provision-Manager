357c2be54ef66ab58c52ce8560f767136cad7d04
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-05-02 02:04:56 +0100

    Update EventServiceProvider.php

--- back-end/app/Providers/EventServiceProvider.php
+++ back-end/app/Providers/EventServiceProvider.php
@@ -16,11 +16,7 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
+
     public function boot()
     {
         //
